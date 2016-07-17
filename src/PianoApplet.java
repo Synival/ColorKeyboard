1e85e86d21d4e067a7c704004f79192f21a9422f
Simon Bielman <simon@simonbielman.com>
2016-07-16 22:01:20 -0700

    Comments and clean-ups: round 1

--- src/PianoApplet.java
+++ src/PianoApplet.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class PianoApplet extends Applet implements ActionListener,
              TextListener
 {
+   // Message displayed in 'help' menu.
    public static final String HELP_MESSAGE =
    "You're probobly getting lots of errors about file permissions when you " +
    "try to play files -- But there's a way to fix it!\n\n" +
@@ -28,68 +29,81 @@ public class PianoApplet extends Applet implements ActionListener,
    "You can add multiple entries to this file to play from multiple " +
    "directories.";
 
-   private Label metronomeOnLabel = new Label ("Off");
+   // Piano layout buttons.
+   private TextField layoutText        = new TextField ("121121211212");
+   private TextField transpositionText = new TextField ("21");
+   private Button rebuildButton        = new Button ("Rebuild Piano");
+   private Button standardButton       = new Button ("Standard");
+   private Button wholetoneButton      = new Button ("Wholetone");
 
+   // Metronome.
+   private Button metronomeButton  = new Button ("Toggle");
+   private Label metronomeOnLabel  = new Label ("Off");
+   private TextField metronomeText = new TextField ("120");
+   private Metronome metronome     = new Metronome ();
+
+   // Instrument patch.
+   private TextField instrumentText = new TextField (3);
+
+   // MIDI control.
+   private TextField midiText = new TextField ("87-15b.mid");
+   private Button playButton  = new Button ("Play");
+   private Button stopButton  = new Button ("Stop");
+   private Button helpButton  = new Button ("Help");
+
+   // Visualizer.
+   private Visual visual  = new Visual (500, 200);
    private Label hueLabel = new Label ("Hue: 360\u00b0");
    private Label satLabel = new Label ("Saturation: 100%");
 
-   private Button metronomeButton = new Button ("Toggle");
-   private Button rebuildButton   = new Button ("Rebuild Piano");
-   private Button standardButton  = new Button ("Standard");
-   private Button wholetoneButton = new Button ("Wholetone");
-   private Button playButton      = new Button ("Play");
-   private Button stopButton      = new Button ("Stop");
-   private Button helpButton      = new Button ("Help");
-
-   private TextField metronomeText     = new TextField ("120");
-   private TextField layoutText        = new TextField ("121121211212");
-   private TextField transpositionText = new TextField ("21");
-   private TextField keycodeText       = new TextField ("Key");
-   private TextField toneText          = new TextField ("Tone");
-   private TextField instrumentText    = new TextField ("0    ");
-   private TextField midiText          = new TextField ("87-15b.mid");
-
-   private Piano piano;
-   private Metronome metronome = new Metronome ();
+   // Piano.
    private Keymap keymap = new Keymap ();
-   private Visual visual = new Visual (500, 200);
+   private Timer timer   = new Timer ();
+   private Piano piano; // (Initialized in constructor)
 
-   private Timer timer = new Timer ();
-
+   // Visualizer statistics (hue/saturation) update routine.
    public class UpdateTask extends TimerTask
    {
       Color lastColor = Color.white;
-
       public void run ()
       {
+         // Has the color changed?  If not, we don't need to do anything.
          Color color = visual.averageColor();
+         if (color.getRGB() == lastColor.getRGB())
+            return;
 
-         if (color.getRGB() != lastColor.getRGB()) {
-            lastColor = color;
+         // Color changed.  Remember its value so we don't have to compute
+         // this again every frame.
+         lastColor = color;
 
-            float[] hsb = new float[3];
+         // Determine HSB.
+         float[] hsb = new float[3];
+         Color.RGBtoHSB (color.getRed(), color.getGreen(),
+                         color.getBlue(), hsb);
 
-            Color.RGBtoHSB (color.getRed(), color.getGreen(),
-                            color.getBlue(), hsb);
-
-            if (hsb[2] == 0) {
-               hueLabel.setText ("Hue: N/A");
-               satLabel.setText ("Saturation: N/A");
-            }
-            else if (hsb[1] == 0) {
-               hueLabel.setText ("Hue: N/A");
-               satLabel.setText ("Saturation: " + (int) (hsb[1] * 100) + "%");
-            }
-            else {
-               hueLabel.setText ("Hue: " + (int) (hsb[0] * 360) + "\u00b0");
-               satLabel.setText ("Saturation: " + (int) (hsb[1] * 100) + "%");
-            }
+         // If brightness is zero, there is no hue/saturation.
+         if (hsb[2] == 0) {
+            hueLabel.setText ("Hue: N/A");
+            satLabel.setText ("Saturation: N/A");
+         }
+         // If saturation is zero, there is no hue.
+         else if (hsb[1] == 0) {
+            hueLabel.setText ("Hue: N/A");
+            satLabel.setText ("Saturation: 0%");
+         }
+         // Otherwise, do some calculations.
+         else {
+            hueLabel.setText ("Hue: " +
+               (int) Math.round (hsb[0] * 360.00) + "\u00b0");
+            satLabel.setText ("Saturation: " +
+               (int) Math.round (hsb[1] * 100.00) + "%");
          }
       }
    }
 
    public void init ()
    {
+      // Create the top row of controls.
       add (new Label ("Key Layout: "));
       add (layoutText);
       add (new Label ("Lowest Note: "));
@@ -100,38 +114,33 @@ public class PianoApplet extends Applet implements ActionListener,
       add (wholetoneButton);
       add (new HorizDivider());
 
+      // Add a metronome and instrument (MIDI patch) setting.
       add (new Label ("Metronome: "));
       add (metronomeOnLabel);
       add (metronomeText);
       add (metronomeButton);
       add (new Label ("Instrument: "));
+      instrumentText.setText ("0");
       add (instrumentText);
       add (new HorizDivider());
 
+      // MIDI playback controls.
       add (midiText);
       add (playButton);
       add (stopButton);
       add (helpButton);
       add (new HorizDivider());
 
+      // Visualizer.
       add (visual);
       add (new HorizDivider());
 
+      // Hue & Saturation reports.
       add (hueLabel);
       add (satLabel);
       add (new HorizDivider());
 
-      try {
-         piano = new Piano(layoutText.getText(), 88,
-                           Integer.parseInt (transpositionText.getText()));
-      }
-      catch (PianoException e) {
-         System.out.println (e);
-         return;
-      }
-
-      add (piano);
-
+      // Route all events to this class for convenience.
       metronomeButton.addActionListener (this);
       rebuildButton.addActionListener (this);
       standardButton.addActionListener (this);
@@ -139,21 +148,41 @@ public class PianoApplet extends Applet implements ActionListener,
       playButton.addActionListener (this);
       stopButton.addActionListener (this);
       helpButton.addActionListener (this);
-
       metronomeText.addTextListener (this);
       instrumentText.addTextListener (this);
 
-      keymap.usePreset (Keymap.STANDARD);
-      piano.assignKeymap (keymap);
-      piano.assignVisual (visual);
+      // Create the piano and print any errors directly to the console.
+      try {
+         // Use the default layout text (121211212121),
+         // 88 keys, and whatever transposition is requested (default = 23).
+         piano = new Piano(layoutText.getText(), 88,
+                           Integer.parseInt (transpositionText.getText()));
+         add (piano);
 
-      timer.scheduleAtFixedRate (new UpdateTask(), 0, 25);
-      piano.requestFocusInWindow ();
+         // Assign our keymap and visualizer to the piano.
+         keymap.usePreset (Keymap.STANDARD);
+         piano.assignKeymap (keymap);
+         piano.assignVisual (visual);
+
+         // Listen to keyboard every 25ms.
+         timer.scheduleAtFixedRate (new UpdateTask(), 0, 25);
+
+         // Focus piano.
+         piano.requestFocusInWindow ();
+      }
+      catch (PianoException e) {
+         System.out.println (e);
+         return;
+      }
    }
 
    public void actionPerformed (ActionEvent event)
    {
-      if (event.getSource() == metronomeButton) {
+      // What triggered this event?
+      Object source = event.getSource ();
+
+      // Toggle the metronome on and off.
+      if (source == metronomeButton) {
          if (metronome.isEnabled()) {
             metronomeOnLabel.setText ("Off");
             metronome.disable ();
@@ -163,77 +192,87 @@ public class PianoApplet extends Applet implements ActionListener,
             metronome.enable (Integer.parseInt (metronomeText.getText()));
          }
       }
-      else if (event.getSource() == rebuildButton) {
+      // "Rebuild Piano" button changes the key layout.
+      else if (source == rebuildButton) {
          try {
             piano.setLayout (layoutText.getText(), 88,
-                             Integer.parseInt (transpositionText.getText()));
+               Integer.parseInt (transpositionText.getText()));
          }
          catch (PianoException e) {
             System.out.println (e);
          }
       }
-      else if (event.getSource() == standardButton) {
+      // "Standard" resets key layout to defaults.
+      else if (source == standardButton) {
          try {
             layoutText.setText ("121121211212");
             transpositionText.setText ("21");
             piano.setLayout (layoutText.getText(), 88,
-                             Integer.parseInt (transpositionText.getText()));
-
+               Integer.parseInt (transpositionText.getText()));
             keymap.usePreset (Keymap.STANDARD);
          }
          catch (PianoException e) {
             System.out.println (e);
          }
       }
-      else if (event.getSource() == wholetoneButton) {
+      // "Wholetone" uses a simple alternating "low, high" layout.
+      else if (source == wholetoneButton) {
          try {
             layoutText.setText ("12");
             transpositionText.setText ("21");
             piano.setLayout (layoutText.getText(), 88,
-                             Integer.parseInt (transpositionText.getText()));
-
+               Integer.parseInt (transpositionText.getText()));
             keymap.usePreset (Keymap.WHOLETONE);
          }
          catch (PianoException e) {
             System.out.println (e);
          }
       }
-      else if (event.getSource() == playButton)
+      // Play MIDI.
+      else if (source == playButton) {
          piano.play (midiText.getText());
-      else if (event.getSource() == stopButton)
+      }
+      // Stop MIDI.
+      else if (source == stopButton) {
          piano.stop ();
-      else if (event.getSource() == helpButton) {
+      }
+      // Help window.
+      else if (source == helpButton) {
          Container container = this.getParent();
-
          while (!(container instanceof Frame))
             container = container.getParent();
          Frame parent = (Frame) container;
 
          HelpDialog d = new HelpDialog (parent, HELP_MESSAGE);
-
          d.show ();
       }
    }
 
    public void textValueChanged (TextEvent event)
    {
-      int bpm, instr;
+      Object source = event.getSource ();
 
+      // If we changed the metronome, update the value.
+      // Use 120 if the value entered is bogus.
       if (event.getSource() == metronomeText && metronome.isEnabled()) {
+         int bpm;
          try {
             bpm = Integer.parseInt (metronomeText.getText ());
          }
          catch (Exception e) {
-            bpm = 1;
+            bpm = 120;
          }
          metronome.enable (bpm);
       }
+      // If the changed the instrument, update our piano's patch.
+      // Use 0 (accoustic grand piano) if the value entered is value.
       else if (event.getSource() == instrumentText) {
+         int instr;
          try {
             instr = Integer.parseInt (instrumentText.getText ());
          }
          catch (Exception e) {
-            instr = 1;
+            instr = 0;
          }
          piano.setMidi (0, instr);
       }
